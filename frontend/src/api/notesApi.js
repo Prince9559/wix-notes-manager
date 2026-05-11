04657ec3f3e77c1c8d28d492569f2892b4acddaa
Prince Software <kumarprince95596186@gmail.com>
2026-05-11 12:54:33 +0530

    project

--- frontend/src/api/notesApi.js
+++ frontend/src/api/notesApi.js
@@ -1,31 +1,33 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/notes";
+const API_URL =
+  "https://wix-notes-manager.onrender.com/api/notes";
 
 
-// GET Notes
 export const getNotes = async () => {
   return await axios.get(API_URL);
 };
 
-
-// ADD Note
 export const addNote = async (noteData) => {
-  return await axios.post(API_URL, noteData);
+  return await axios.post(
+    API_URL,
+    noteData
+  );
 };
 
+export const updateNote = async (
+  id,
+  noteData
+) => {
 
-// UPDATE Note
-export const updateNote = async (id, noteData) => {
   return await axios.put(
     `${API_URL}/${id}`,
     noteData
   );
 };
 
-
-// DELETE Note
 export const deleteNote = async (id) => {
+
   return await axios.delete(
     `${API_URL}/${id}`
   );
