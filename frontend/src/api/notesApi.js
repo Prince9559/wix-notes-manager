540909ce3efd338cb4943955287cc1507994bb4f
Prince Software <kumarprince95596186@gmail.com>
2026-06-28 12:46:17 +0530

    project

--- frontend/src/api/notesApi.js
+++ frontend/src/api/notesApi.js
@@ -4,13 +4,10 @@ const API_URL =
   "https://wix-notes-manager.onrender.com/api/notes";
 
 
-// GET Notes
 export const getNotes = async () => {
   return await axios.get(API_URL);
 };
 
-
-// ADD Note
 export const addNote = async (noteData) => {
   return await axios.post(
     API_URL,
@@ -19,7 +16,6 @@ export const addNote = async (noteData) => {
 };
 
 
-// UPDATE Note
 export const updateNote = async (
   id,
   noteData
@@ -32,7 +28,6 @@ export const updateNote = async (
 };
 
 
-// DELETE Note
 export const deleteNote = async (id) => {
 
   return await axios.delete(
